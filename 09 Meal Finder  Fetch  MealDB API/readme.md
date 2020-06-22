6003f3df3bb065b716354a685bf3abda27ad2702
eslessons <66363651+eslessons@users.noreply.github.com>
2020-06-22 17:05:29 +0300

    Project 09 is almost finished, trying to put image in readme.md

--- 09 Meal Finder  Fetch  MealDB API/readme.md
+++ 09 Meal Finder  Fetch  MealDB API/readme.md
@@ -9,3 +9,6 @@ Search and generate random meals from the [themealdb.com](www.themealdb.com) API
 - Display meals in DOM with image and hover effect
 - Click on meal and see the details
 - Click on generate button and fetch & display a random meal
+
+![GitHub Logo](/images/Screenshot_Meal_Finder.png)
+Format: ![Alt Text](url)
---
+++ 09 Meal Finder  Fetch  MealDB API/script.js
@@ -0,0 +1,109 @@
+const search = document.getElementById('search'),
+  submit = document.getElementById('submit'),
+  random = document.getElementById('random'),
+  mealsEl = document.getElementById('meals'),
+  resultHeading = document.getElementById('result-heading'),
+  single_mealEl = document.getElementById('single-meal');
+
+// Search meal and fetch from API
+function searchMeal(e) {
+  e.preventDefault();
+
+  // Clear single meal
+  single_mealEl.innerHTML = '';
+
+  // Get search term
+  const term = search.value;
+
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term.trim()}`)
+      .then((res) => res.json())
+      .then((data) => {
+        // Log data and clear search text
+        console.log(data);
+        search.value = '';
+
+        resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`;
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              (meal) => `
+          <div class="meal">
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+            <div class="meal-info" data-mealID="${meal.idMeal}"><h3>${meal.strMeal}</h3></div>
+          </div>
+          `
+            )
+            .join('');
+        }
+      });
+  } else {
+    alert('Please enter a search term');
+  }
+}
+
+// Fetch meal by ID
+function getMealByID(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDom(meal);
+    });
+}
+
+// Add meal to DOM
+function addMealToDom(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  single_mealEl.innerHTML = `
+  <div class="single-meal">
+    <h1>${meal.strMeal}</h1>
+    <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+    <div class="single-meal-info">
+      ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+      ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+    </div>
+    <div class="main">
+      <p>${meal.strInstructions}</p>
+      <h2>Ingredients</h2>
+      <ul>
+        ${ingredients.map((ingt) => `<li>${ingt}</li>`).join('')}
+    </div>
+  </div>
+  `;
+}
+
+// Event Listeners
+submit.addEventListener('submit', searchMeal);
+
+mealsEl.addEventListener('click', (e) => {
+  const path = e.path || (e.composedPath && e.composedPath());
+
+  const mealInfo = path.find((item) => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
+    }
+  });
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealid');
+    getMealByID(mealID);
+  }
+});
